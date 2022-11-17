e526a9cab8620738e6bb2175f2cf432e08321f9b
arya065 <arya065@g.educaand.es>
2022-11-17 12:39:46 +0100

    работа в классе
    методы в классах, в мейне просто вызов классов и их результатов

--- src/main/java/com/mycompany/tarea4calexey/ClasePersona.java
+++ src/main/java/com/mycompany/tarea4calexey/ClasePersona.java
@@ -1,27 +1,75 @@
 package com.mycompany.tarea4calexey;
 
+import java.util.*;
+
 public class ClasePersona {
+
     private String nombre;
     private int edad;
-    private int nif;
-    private String sexo;
+    private String nif;
+    private char sexo;
     private int peso;
     private int altura;
-    
-    public ClasePersona(){//конструктор с ничем
-        
+
+    public ClasePersona() {//конструктор с ничем
+
     }
-    public ClasePersona(String nombre,int edad, String sexo, int altura,int peso){
-        this.nombre= nombre;
-        this.edad=edad;
-        this.sexo=sexo;
-        this.altura=altura;
-        this.peso=peso;
-        this.nif=nif();
+
+    public ClasePersona(String nombre, int edad, char sexo, int altura, int peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+        this.nif = generaNif();
+        this.sexo = sexo;
+        if (!filtrarSexo(sexo)) {
+            this.sexo = asignarSexo(sexo);
+        }
+
     }
-    private int nif(){
-        int nif=0;
+
+
+
+    private static String generaNif() {
+        Random rand = new Random();
+
+        int numRand = rand.nextInt(99999999 + 1);
+        String nif = "" + numRand;
+        numRand = Math.round(numRand % 23);
+        String letras = "TRWAGMYFPDXBNJZSQVHLCK";
+        char letraNif = letras.charAt(numRand);
+        nif += letraNif;
+        System.out.println(nif);
         return nif;
     }
-}
 
+    private boolean filtrarSexo(char sexo) {
+        return (sexo == 'H' || sexo == 'M' || sexo == 'O');
+    }
+
+    private char asignarSexo(char sexo) {
+        boolean result= filtrarSexo(sexo);
+        if (!result){
+            sexo='O';
+        }
+        return sexo;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        return "ClasePersona{" + "nombre=" + nombre + ", edad=" + edad + ", nif=" + nif + ", sexo=" + sexo + ", peso=" + peso + ", altura=" + altura + '}';
+    }
+
+    
+    
+}
--- src/main/java/com/mycompany/tarea4calexey/Tarea4cAlexey.java
+++ src/main/java/com/mycompany/tarea4calexey/Tarea4cAlexey.java
@@ -1,8 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.tarea4calexey;
 
 /**
@@ -10,8 +9,9 @@ package com.mycompany.tarea4calexey;
  * @author alexey
  */
 public class Tarea4cAlexey {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ClasePersona person1= new ClasePersona("fgfghf", 1, 'M', 1, 1);
+        System.out.println(person1);
     }
+    
 }
